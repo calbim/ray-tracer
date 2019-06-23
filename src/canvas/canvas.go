f3e974071334cbdfff00457b481bd7f87906308a
calculatingbimbo <catastrophewaitress@outlook.com>
2019-06-22 17:25:10 -0700

    canvas to PPM

--- src/canvas/canvas.go
+++ src/canvas/canvas.go
@@ -2,6 +2,8 @@ package canvas
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 	"strings"
 
 	"../tuple"
@@ -16,18 +18,18 @@ type Canvas struct {
 
 // New returns a new Canvas with width w and height h
 func New(w, h int) Canvas {
-	pixels := make([][]tuple.Tuple, w)
-	for i := 0; i < w; i++ {
-		pixels[i] = make([]tuple.Tuple, h)
+	pixels := make([][]tuple.Tuple, h)
+	for i := 0; i < h; i++ {
+		pixels[i] = make([]tuple.Tuple, w)
 	}
 	return Canvas{
 		w, h, pixels,
 	}
 }
 
-// WritePixel writes pixel p at position x,y on a canvas c
+// WritePixel writes pixel p at position width x, height y on a canvas c
 func WritePixel(c *Canvas, x int, y int, p tuple.Tuple) {
-	c.pixels[x][y] = p
+	c.pixels[y][x] = p
 }
 
 // ToPPM returns the PPM string representation of a canvas
@@ -36,5 +38,57 @@ func ToPPM(c Canvas) string {
 	b.WriteString("P3\n")
 	b.WriteString(fmt.Sprintf("%d %d\n", c.width, c.height))
 	b.WriteString(fmt.Sprintf("%d\n", 255))
-	return b.String()
+	for i := 0; i < c.height; i++ {
+		for j := 0; j < c.width; j++ {
+			pix := ToPixel(c.pixels[i][j])
+			b.WriteString(pix)
+			if j == c.width-1 {
+				b.WriteString("\n")
+			} else {
+				b.WriteString(" ")
+			}
+		}
+	}
+	s := b.String()
+	return s
+	// res := ""
+	// line := 0
+	// for i := 0; i < len(s); {
+	// 	pix := string(s[i : i+3])
+	// 	res = res + pix
+	// 	line = line + 3
+	// 	i = i + 3
+	// 	if line >= 67 {
+	// 		res = res + "\n"
+	// 	}
+	// 	wspace := string(s[i])
+	// 	if wspace == " " {
+	// 		res = res + wspace
+	// 	}
+
+	// }
+	// return b.String()
+}
+
+// ToPixel converts a color tuple to an string of 3 integers
+// each lying between 0 and 255
+func ToPixel(color tuple.Tuple) string {
+	r := ColorToString(color.X)
+	b := ColorToString(color.Y)
+	g := ColorToString(color.Z)
+
+	return r + " " + b + " " + g
+}
+
+// ColorToString converts a float64 representing a hue (r, b or g)
+// to a number lying between 0 and 255
+func ColorToString(c float64) string {
+	i := math.Round(c * 255)
+	if i > 255 {
+		i = 255
+	}
+	if i < 0 {
+		i = 0
+	}
+	return strconv.Itoa(int(i))
 }
--- src/canvas/canvas_test.go
+++ src/canvas/canvas_test.go
@@ -1,7 +1,6 @@
 package canvas
 
 import (
-	"fmt"
 	"strings"
 	"testing"
 
@@ -13,8 +12,8 @@ func TestCanvas(t *testing.T) {
 	if c.width != 10 || c.height != 20 {
 		t.Errorf("Canvas width and height should be 10 and 20 respectively")
 	}
-	for i := 0; i < c.width; i++ {
-		for j := 0; j < c.height; j++ {
+	for i := 0; i < c.height; i++ {
+		for j := 0; j < c.width; j++ {
 			if !tuple.Equals(c.pixels[i][j], tuple.Color(0, 0, 0)) {
 				t.Errorf("Every pixel of a new canvas should be black")
 			}
@@ -26,16 +25,23 @@ func TestWriteToCanvas(t *testing.T) {
 	c := New(10, 20)
 	r := tuple.Color(1, 0, 0)
 	WritePixel(&c, 2, 3, r)
-	if !tuple.Equals(c.pixels[2][3], r) {
-		t.Errorf("Pixel at (2,3) should be red")
+	if !tuple.Equals(c.pixels[3][2], r) {
+		t.Errorf("Pixel at width 2 amd height 3 should be red")
 	}
 }
-
 func TestCanvastoPPM(t *testing.T) {
 	c := New(5, 3)
+
+	c1 := tuple.Color(1.5, 0, 0)
+	c2 := tuple.Color(0, 0.5, 0)
+	c3 := tuple.Color(-0.5, 0, 1)
+
+	WritePixel(&c, 0, 0, c1)
+	WritePixel(&c, 2, 1, c2)
+	WritePixel(&c, 4, 2, c3)
+
 	ppm := ToPPM(c)
 	ppmSplit := strings.Split(ppm, "\n")
-	fmt.Println(ppmSplit)
 	if ppmSplit[0] != "P3" {
 		t.Errorf("First line of ppm header should be P3")
 	}
@@ -45,4 +51,38 @@ func TestCanvastoPPM(t *testing.T) {
 	if ppmSplit[2] != "255" {
 		t.Errorf("Third line of ppm header should be 255")
 	}
+	if ppmSplit[3] != "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0" {
+		t.Errorf("Line 1: Incorect PPM conversion")
+	}
+	if ppmSplit[4] != "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0" {
+		t.Errorf("Line 2: Incorect PPM conversion")
+	}
+	if ppmSplit[5] != "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255" {
+		t.Errorf("Line 3: Incorect PPM conversion")
+	}
 }
+
+// func TestSplitLongLinesinPPM(t *testing.T) {
+// 	c := New(10, 2)
+// 	color := tuple.Color(1, 0.8, 0.6)
+// 	for i := 0; i < 10; i++ {
+// 		for j := 0; j < 2; j++ {
+// 			WritePixel(&c, i, j, color)
+// 		}
+// 	}
+// 	ppm := ToPPM(c)
+// 	ppmSplit := strings.Split(ppm, "\n")
+// 	if ppmSplit[3] != "255 204 153 255 204 153 255 204 153 255 204 153 255 204" {
+// 		t.Errorf("Incorrect splitting of long strings")
+// 	}
+// 	if ppmSplit[4] != "153 255 204 153 255 204 153 255 204 153 255 204 153" {
+// 		t.Errorf("Incorrect splitting of long strings")
+// 	}
+// 	if ppmSplit[5] != "255 204 153 255 204 153 255 204 153 255 204 153 255 204" {
+// 		t.Errorf("Incorrect splitting of long strings")
+// 	}
+// 	if ppmSplit[6] != "153 255 204 153 255 204 153 255 204 153 255 204 153" {
+// 		t.Errorf("Incorrect splitting of long strings")
+// 	}
+
+// }
