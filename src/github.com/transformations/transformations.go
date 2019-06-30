7293d590877d13247451cfde728238011e4cf357
calbim <catastrophewaitress@outlook.com>
2019-06-30 16:13:39 -0700

    rotation

---
+++ src/github.com/transformations/transformations.go
@@ -0,0 +1,56 @@
+package transformations
+
+import (
+	"math"
+
+	"../matrix"
+)
+
+// Pi = 3.14
+var Pi = math.Pi
+
+// NewTranslation returns a new 4x4 transformation matrix
+func NewTranslation(x, y, z float64) [][]float64 {
+	return matrix.New([]float64{1, 0, 0, x, 0, 1, 0, y, 0, 0, 1, z, 0, 0, 0, 1}, 4, 4)
+}
+
+// NewScaling returns a new 4x4 scaling matrix
+func NewScaling(x, y, z float64) [][]float64 {
+	return matrix.New([]float64{
+		x, 0, 0, 0,
+		0, y, 0, 0,
+		0, 0, z, 0,
+		0, 0, 0, 1},
+		4, 4)
+
+}
+
+// RotationX returns a matrix that represents a rotation by r radians around the X axis
+func RotationX(r float64) [][]float64 {
+	return matrix.New([]float64{
+		1, 0, 0, 0,
+		0, math.Cos(r), -math.Sin(r), 0,
+		0, math.Sin(r), math.Cos(r), 0,
+		0, 0, 0, 1,
+	}, 4, 4)
+}
+
+// RotationY returns a matrix that represents a rotation by r radians around the Y axis
+func RotationY(r float64) [][]float64 {
+	return matrix.New([]float64{
+		math.Cos(r), 0, math.Sin(r), 0,
+		0, 1, 0, 0,
+		-math.Sin(r), 0, math.Cos(r), 0,
+		0, 0, 0, 1,
+	}, 4, 4)
+}
+
+// RotationZ returns a matrix that represents a rotation by r radians around the Z axis
+func RotationZ(r float64) [][]float64 {
+	return matrix.New([]float64{
+		math.Cos(r), -math.Sin(r), 0, 0,
+		math.Sin(r), math.Cos(r), 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1,
+	}, 4, 4)
+}
---
+++ src/github.com/transformations/transformations_test.go
@@ -0,0 +1,121 @@
+package transformations
+
+import (
+	"math"
+	"testing"
+
+	"../matrix"
+	"../tuple"
+)
+
+func TestTranslation(t *testing.T) {
+	transform := NewTranslation(5, -3, 2)
+	p := tuple.Point(-3, 4, 5)
+	if !tuple.Equals(matrix.MultiplyWithTuple(transform, p), tuple.Point(2, 1, 7)) {
+		t.Errorf("Transformation is wrong!")
+	}
+}
+
+func TestTranslationInverse(t *testing.T) {
+	transform := NewTranslation(5, -3, 2)
+	inv, err := matrix.Inverse(transform, 4)
+	if err != nil {
+		t.Errorf("could not inverse transformation matrix")
+	}
+	p := tuple.Point(-3, 4, 5)
+	if !tuple.Equals(matrix.MultiplyWithTuple(inv, p), tuple.Point(-8, 7, 3)) {
+		t.Errorf("Transformation is wrong!")
+	}
+}
+
+func TestTranslateVector(t *testing.T) {
+	transform := NewTranslation(5, -3, 2)
+	v := tuple.Vector(-3, 4, 5)
+	if !tuple.Equals(matrix.MultiplyWithTuple(transform, v), v) {
+		t.Errorf("Transformation of a vector returns vector itself")
+	}
+}
+
+func TestScalePoint(t *testing.T) {
+	transform := NewScaling(2, 3, 4)
+	p := tuple.Point(-4, 6, 8)
+	if !tuple.Equals(matrix.MultiplyWithTuple(transform, p), tuple.Point(-8, 18, 32)) {
+		t.Errorf("Scaling is incorrect")
+	}
+}
+
+func TestScaleVector(t *testing.T) {
+	transform := NewScaling(2, 3, 4)
+	v := tuple.Vector(-4, 6, 8)
+	if !tuple.Equals(matrix.MultiplyWithTuple(transform, v), tuple.Vector(-8, 18, 32)) {
+		t.Errorf("Scaling is incorrect")
+	}
+}
+
+func TestScalingInverse(t *testing.T) {
+	transform := NewScaling(2, 3, 4)
+	inv, err := matrix.Inverse(transform, 4)
+	if err != nil {
+		t.Errorf("could not inverse transformation matrix")
+	}
+	v := tuple.Vector(-4, 6, 8)
+	if !tuple.Equals(matrix.MultiplyWithTuple(inv, v), tuple.Vector(-2, 2, 2)) {
+		t.Errorf("Scaling is incorrect!")
+	}
+}
+
+func TestReflection(t *testing.T) {
+	transform := NewScaling(-1, 1, 1)
+	p := tuple.Point(2, 3, 4)
+	if !tuple.Equals(matrix.MultiplyWithTuple(transform, p), tuple.Point(-2, 3, 4)) {
+		t.Errorf("Reflection is incorrect!")
+	}
+}
+
+func TestXRotation(t *testing.T) {
+	p := tuple.Point(0, 1, 0)
+	halfQuarter := RotationX(Pi / 4)
+	fullQuarter := RotationX(Pi / 2)
+	if !tuple.Equals(matrix.MultiplyWithTuple(halfQuarter, p), tuple.Point(0, math.Sqrt(2)/2, math.Sqrt(2)/2)) {
+		t.Errorf("rotation for p %v is incorrect", p)
+	}
+	if !tuple.Equals(matrix.MultiplyWithTuple(fullQuarter, p), tuple.Point(0, 0, 1)) {
+		t.Errorf("rotation for p %v is incorrect", p)
+	}
+}
+
+func TestXRotationInverse(t *testing.T) {
+	p := tuple.Point(0, 1, 0)
+	halfQuarter := RotationX(Pi / 4)
+	inv, err := matrix.Inverse(halfQuarter, 4)
+	if err != nil {
+		t.Errorf("could not compute inverse")
+	}
+	if !tuple.Equals(matrix.MultiplyWithTuple(inv, p), tuple.Point(0, math.Sqrt(2)/2, -math.Sqrt(2)/2)) {
+		t.Errorf("rotation for p %v is incorrect", p)
+	}
+}
+
+func TestYRotation(t *testing.T) {
+	p := tuple.Point(0, 0, 1)
+	halfQuarter := RotationY(Pi / 4)
+	fullQuarter := RotationY(Pi / 2)
+	if !tuple.Equals(matrix.MultiplyWithTuple(halfQuarter, p), tuple.Point(math.Sqrt(2)/2, 0, math.Sqrt(2)/2)) {
+		t.Errorf("rotation for p %v is incorrect", p)
+	}
+	if !tuple.Equals(matrix.MultiplyWithTuple(fullQuarter, p), tuple.Point(1, 0, 0)) {
+		t.Errorf("rotation for p %v is incorrect", p)
+	}
+}
+
+func TestZRotation(t *testing.T) {
+	p := tuple.Point(0, 1, 0)
+	halfQuarter := RotationZ(Pi / 4)
+	fullQuarter := RotationZ(Pi / 2)
+	if !tuple.Equals(matrix.MultiplyWithTuple(halfQuarter, p), tuple.Point(-math.Sqrt(2)/2, math.Sqrt(2)/2, 0)) {
+		t.Errorf("rotation for p %v is incorrect", p)
+	}
+	if !tuple.Equals(matrix.MultiplyWithTuple(fullQuarter, p), tuple.Point(-1, 0, 0)) {
+		t.Errorf("rotation for p %v is incorrect", p)
+	}
+}
