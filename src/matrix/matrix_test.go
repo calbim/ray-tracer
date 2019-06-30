42178a09d1354316370e7d726052f642d47052b9
calbim <catastrophewaitress@outlook.com>
2019-06-30 10:04:04 -0700

    add more tests for inversion

--- src/matrix/matrix_test.go
+++ src/matrix/matrix_test.go
@@ -222,4 +222,24 @@ func TestInverse(t *testing.T) {
 	if !Equals(b, expected, 4, 4, 4, 4) {
 		t.Errorf("Matrix should be %v, is %v", expected, b)
 	}
+
+	m = New([]float64{8, -5, 9, 2, 7, 5, 6, 1, -6, 0, 9, 6, -3, 0, -9, -4}, 4, 4)
+	b, err = Inverse(m, 4)
+	if err != nil {
+		t.Errorf("error while inversing matrix")
+	}
+	expected = New([]float64{-0.15385, -0.15385, -0.28205, -0.53846, -0.07692, 0.12308, 0.02564, 0.03077, 0.35897, 0.35897, 0.43590, 0.92308, -0.69231, -0.69231, -0.76923, -1.92308}, 4, 4)
+	if !Equals(b, expected, 4, 4, 4, 4) {
+		t.Errorf("Matrix should be %v, is %v", expected, b)
+	}
+
+	m = New([]float64{9, 3, 0, 9, -5, -2, -6, -3, -4, 9, 6, 4, -7, 6, 6, 2}, 4, 4)
+	b, err = Inverse(m, 4)
+	if err != nil {
+		t.Errorf("error while inversing matrix")
+	}
+	expected = New([]float64{-0.04074, -0.07778, 0.14444, -0.22222, -0.07778, 0.03333, 0.36667, -0.33333, -0.02901, -0.14630, -0.10926, 0.12963, 0.17778, 0.06667, -0.26667, 0.33333}, 4, 4)
+	if !Equals(b, expected, 4, 4, 4, 4) {
+		t.Errorf("Matrix should be %v\n, is %v", expected, b)
+	}
 }
