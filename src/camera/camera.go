2431bef1c34b88f70168577dd179afef892cf875
calbim <catastrophewaitress@outlook.com>
2019-07-21 15:59:11 -0700

    camera

---
+++ src/camera/camera.go
@@ -0,0 +1,77 @@
+package camera
+
+import (
+	"errors"
+	"math"
+
+	"github.com/calbim/ray-tracer/src/ray"
+	"github.com/calbim/ray-tracer/src/tuple"
+
+	"github.com/calbim/ray-tracer/src/matrix"
+)
+
+// Camera represents a camera
+type Camera struct {
+	HSize       float64 //in pixels
+	VSize       float64
+	FieldOfView float64
+	Transform   [][]float64
+	PixelSize   float64
+	HalfHeight  float64
+	HalfWidth   float64
+}
+
+// New returns a new camera
+func New(hSize float64, vSize float64, fieldOfView float64) Camera {
+	transform := matrix.NewIdentity()
+	c := Camera{
+		HSize:       hSize,
+		VSize:       vSize,
+		FieldOfView: fieldOfView,
+		Transform:   transform,
+	}
+	halfView := math.Tan(fieldOfView / 2)
+	aspect := float64(hSize / vSize)
+	if aspect >= 1 {
+		c.HalfWidth = halfView
+		c.HalfHeight = halfView / aspect
+	} else {
+		c.HalfWidth = halfView * aspect
+		c.HalfHeight = halfView
+	}
+	c.PixelSize = c.HalfWidth * 2 / c.HSize
+	return c
+}
+
+//RayForPixel returns the ray from the camera to point (x,y) on canvas
+func RayForPixel(c Camera, x, y int) (*ray.Ray, error) {
+	xOffset := (float64(x) + 0.5) * c.PixelSize
+	yOffset := (float64(y) + 0.5) * c.PixelSize
+	worldX := c.HalfWidth - xOffset
+	worldY := c.HalfHeight - yOffset
+	inverse, err := matrix.Inverse(c.Transform, 4)
+	if err != nil {
+		return nil, errors.New("could not compute inverse for matrix")
+	}
+	pixel := matrix.MultiplyWithTuple(inverse, tuple.Point(worldX, worldY, -1))
+	origin := matrix.MultiplyWithTuple(inverse, tuple.Point(0, 0, 0))
+	direction := tuple.Normalize(tuple.Subtract(pixel, origin))
+	return &ray.Ray{
+		Origin:    origin,
+		Direction: direction,
+	}, nil
+}
+
+func pixelSize(hSize int, vSize int, fieldOfView float64) float64 {
+	halfView := math.Tan(fieldOfView / 2)
+	aspect := float64(hSize / vSize)
+	var halfWidth, halfHeight float64
+	if aspect >= 1 {
+		halfWidth = halfView
+		halfHeight = halfView / aspect
+	} else {
+		halfWidth = halfView * aspect
+		halfHeight = halfView
+	}
+	return halfWidth * 2 / halfHeight
+}
---
+++ src/camera/camera_test.go
@@ -0,0 +1,89 @@
+package camera
+
+import (
+	"math"
+	"testing"
+
+	"github.com/calbim/ray-tracer/src/transformations"
+
+	"github.com/calbim/ray-tracer/src/tuple"
+
+	"github.com/calbim/ray-tracer/src/matrix"
+)
+
+func TestConstructCamera(t *testing.T) {
+	hSize := 160.0
+	vSize := 120.0
+	fieldOfView := math.Pi / 2
+	c := New(hSize, vSize, fieldOfView)
+	if c.HSize != 160 {
+		t.Errorf("Horizontal size of canvas should be %v, but it is %v", hSize, c.HSize)
+	}
+	if c.VSize != 120 {
+		t.Errorf("Vertical size of canvas should be %v, but it is %v", vSize, c.VSize)
+	}
+	if c.FieldOfView != math.Pi/2 {
+		t.Errorf("Field of view of camera should be %v, but it is %v", fieldOfView, c.FieldOfView)
+
+	}
+	if !matrix.Equals(c.Transform, matrix.NewIdentity(), 4, 4, 4, 4) {
+		t.Errorf("Transform should be %v, but it is %v", matrix.NewIdentity(), c.Transform)
+	}
+}
+
+func TestPixelSizeHorizontalCanvas(t *testing.T) {
+	c := New(200, 125, math.Pi/2)
+	if c.PixelSize != 0.01 {
+		t.Errorf("Pixel size should be %v, but it is %v", 0.01, c.PixelSize)
+	}
+}
+
+func TestPixelSizeVerticalCanvas(t *testing.T) {
+	c := New(125, 200, math.Pi/2)
+	if c.PixelSize != 0.01 {
+		t.Errorf("Pixel size should be %v, but it is %v", 0.01, c.PixelSize)
+	}
+}
+
+func TestConstructRayCenterOfCanvas(t *testing.T) {
+	c := New(201, 101, math.Pi/2)
+	r, err := RayForPixel(c, 100, 50)
+	if err != nil {
+		t.Errorf("Error %v while calculating ray for pixel", err)
+	}
+	if !tuple.Equals(r.Origin, tuple.Point(0, 0, 0)) {
+		t.Errorf("Ray origin should be %v, but it is %v", tuple.Point(0, 0, 0), r.Origin)
+	}
+	if !tuple.Equals(r.Direction, tuple.Vector(0, 0, -1)) {
+		t.Errorf("Ray vector should be %v, but it is %v", tuple.Vector(0, 0, -1), r.Direction)
+	}
+}
+
+func TestConstructRayCornerOfCanvas(t *testing.T) {
+	c := New(201, 101, math.Pi/2)
+	r, err := RayForPixel(c, 0, 0)
+	if err != nil {
+		t.Errorf("Error %v while calculating ray for pixel", err)
+	}
+	if !tuple.Equals(r.Origin, tuple.Point(0, 0, 0)) {
+		t.Errorf("Ray origin should be %v, but it is %v", tuple.Point(0, 0, 0), r.Origin)
+	}
+	if !tuple.Equals(r.Direction, tuple.Vector(0.66519, 0.33259, -0.66851)) {
+		t.Errorf("Ray vector should be %v, but it is %v", tuple.Vector(0.66519, 0.33259, -0.66851), r.Direction)
+	}
+}
+
+func TestConstructRayWhenCameraIsTransformed(t *testing.T) {
+	c := New(201, 101, math.Pi/2)
+	c.Transform = matrix.Multiply(transformations.RotationY(math.Pi/4), transformations.NewTranslation(0, -2, 5))
+	r, err := RayForPixel(c, 100, 50)
+	if err != nil {
+		t.Errorf("Error %v while calculating ray for pixel", err)
+	}
+	if !tuple.Equals(r.Origin, tuple.Point(0, 2, -5)) {
+		t.Errorf("Ray origin should be %v, but it is %v", tuple.Point(0, 2, -5), r.Origin)
+	}
+	if !tuple.Equals(r.Direction, tuple.Vector(math.Sqrt(2)/2, 0, -math.Sqrt(2)/2)) {
+		t.Errorf("Ray vector should be %v, but it is %v", tuple.Vector(math.Sqrt(2)/2, 0, -math.Sqrt(2)/2), r.Direction)
+	}
+}
