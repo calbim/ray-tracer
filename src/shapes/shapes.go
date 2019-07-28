3c87d9f96c701b0987e2409d7f1a341d85679252
calbim <catastrophewaitress@outlook.com>
2019-07-28 15:35:48 -0700

    shape normal

--- src/shapes/shapes.go
+++ src/shapes/shapes.go
@@ -1,8 +1,15 @@
 package shapes
 
 import (
+	"errors"
+	"fmt"
+
+	"github.com/calbim/ray-tracer/src/tuple"
+
+	"github.com/calbim/ray-tracer/src/intersections"
 	"github.com/calbim/ray-tracer/src/material"
 	"github.com/calbim/ray-tracer/src/matrix"
+	"github.com/calbim/ray-tracer/src/ray"
 )
 
 // Shape interface
@@ -11,12 +18,17 @@ type Shape interface {
 	GetTransform() [][]float64
 	GetMaterial() material.Material
 	SetMaterial(material.Material)
+	GetSavedRay() ray.Ray
+	SetSavedRay(r ray.Ray)
+	Intersect() ([]intersections.Intersection, error)
+	Normal(tuple.Tuple) (tuple.Tuple, error)
 }
 
 // TestShape is an object of type Shape
 type TestShape struct {
 	transform [][]float64
 	material  material.Material
+	savedRay  ray.Ray
 }
 
 // GetTransform returns transform for shape
@@ -39,10 +51,61 @@ func (ts *TestShape) SetMaterial(m material.Material) {
 	ts.material = m
 }
 
+// GetSavedRay returns transform for shape
+func (ts *TestShape) GetSavedRay() ray.Ray {
+	return ts.savedRay
+}
+
+// SetSavedRay sets t transform for shape
+func (ts *TestShape) SetSavedRay(r ray.Ray) {
+	ts.savedRay = r
+}
+
+// Intersect returns the intersections when ray intersects shape
+func (ts *TestShape) Intersect() ([]intersections.Intersection, error) {
+	return []intersections.Intersection{}, nil
+}
+
+// Normal calculates a normal at point p
+func (ts *TestShape) Normal(p tuple.Tuple) (tuple.Tuple, error) {
+	return tuple.Vector(p.X, p.Y, p.Z), nil
+}
+
 // NewTestShape returns a new TestShape
 func NewTestShape() *TestShape {
 	return &TestShape{
 		transform: matrix.NewIdentity(),
 		material:  material.New(),
 	}
+}
+
+// Intersect returns the intersections when a ray intersects a shape
+func Intersect(s Shape, r ray.Ray) ([]intersections.Intersection, error) {
+	inv, err := matrix.Inverse(s.GetTransform(), 4)
+	if err != nil {
+		return nil, fmt.Errorf("Could not calculate matrix inverse %v", err)
+	}
+	s.SetSavedRay(ray.Ray{
+		Origin:    matrix.MultiplyWithTuple(inv, r.Origin),
+		Direction: matrix.MultiplyWithTuple(inv, r.Direction),
+	})
+	return s.Intersect()
+}
+
+// Normal calculates the normal at point p for shape s
+func Normal(s Shape, p tuple.Tuple) (*tuple.Tuple, error) {
+	inverse, err := matrix.Inverse(s.GetTransform(), 4)
+	if err != nil {
+		return nil, errors.New("Could not compute object point for world point")
+	}
+	localPoint := matrix.MultiplyWithTuple(inverse, p)
+	localNormal, err := s.Normal(localPoint)
+	if err != nil {
+		return nil, fmt.Errorf("Could not calculate normal because %v", err)
+	}
+	tuple.Subtract(localPoint, tuple.Point(0, 0, 0))
+	worldNormal := matrix.MultiplyWithTuple(matrix.Transpose(inverse), localNormal)
+	worldNormal.W = 0
+	normalized := tuple.Normalize(worldNormal)
+	return &normalized, nil
 }
--- src/shapes/shapes_test.go
+++ src/shapes/shapes_test.go
@@ -3,6 +3,9 @@ package shapes
 import (
 	"testing"
 
+	"github.com/calbim/ray-tracer/src/ray"
+	"github.com/calbim/ray-tracer/src/tuple"
+
 	"github.com/calbim/ray-tracer/src/material"
 
 	"github.com/calbim/ray-tracer/src/transformations"
@@ -33,12 +36,58 @@ func TestShapeHasDefaultMaterial(t *testing.T) {
 	}
 }
 
-func TestAssignMaterialToSphere(t *testing.T) {
+func TestAssignMaterialToShape(t *testing.T) {
 	s := NewTestShape()
 	m := material.New()
 	m.Ambient = 1
 	s.SetMaterial(m)
 	if m != s.GetMaterial() {
 		t.Errorf("Shape material should be %v, but is %v", m, s.GetMaterial())
+	}
+}
+
+func TestIntersectScaledShape(t *testing.T) {
+	r := ray.Ray{
+		Origin:    tuple.Point(0, 0, -5),
+		Direction: tuple.Vector(0, 0, 1),
+	}
+	s := NewTestShape()
+	s.SetTransform(transformations.NewScaling(2, 2, 2))
+	_, err := Intersect(s, r)
+	if err != nil {
+		t.Errorf("Error while calculating intersection")
+	}
+	sr := s.GetSavedRay()
+	if !tuple.Equals(sr.Origin, tuple.Point(0, 0, -2.5)) {
+		t.Errorf("Ray origin should be %v but it is %v", tuple.Point(0, 0, -2.5), sr.Origin)
+	}
+}
+
+func TestTranslatedShape(t *testing.T) {
+	r := ray.Ray{
+		Origin:    tuple.Point(0, 0, -5),
+		Direction: tuple.Vector(0, 0, 1),
+	}
+	s := NewTestShape()
+	s.SetTransform(transformations.NewTranslation(5, 0, 0))
+	_, err := Intersect(s, r)
+	if err != nil {
+		t.Errorf("Error while calculating intersection")
+	}
+	sr := s.GetSavedRay()
+	if !tuple.Equals(sr.Origin, tuple.Point(-5, 0, -5)) {
+		t.Errorf("Ray origin should be %v but it is %v", tuple.Point(-5, 0, -5), sr.Origin)
+	}
+}
+
+func TestNormalOnTranslatedSphere(t *testing.T) {
+	s := NewTestShape()
+	s.SetTransform(transformations.NewTranslation(0, 1, 0))
+	n, err := s.Normal(tuple.Point(0, 0.70711, -1.70711))
+	if err != nil {
+		t.Errorf("Could not find normal due to error %v", err)
+	}
+	if !tuple.Equals(n, tuple.Vector(0, 0.70711, -1.70711)) {
+		t.Errorf("Normal should be %v", tuple.Vector(0, 0.70711, -1.70711))
 	}
 }
