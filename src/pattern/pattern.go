7c19ce57dcfaa61936229ac7dd4016cd63b8ca03
calbim <catastrophewaitress@outlook.com>
2019-08-18 20:17:23 -0700

    add tests for pattern transforms

--- src/pattern/pattern.go
+++ src/pattern/pattern.go
@@ -4,19 +4,33 @@ import (
 	"math"
 
 	"github.com/calbim/ray-tracer/src/color"
+	"github.com/calbim/ray-tracer/src/matrix"
 
 	"github.com/calbim/ray-tracer/src/tuple"
 )
 
 //Pattern struct
 type Pattern struct {
-	a color.Color
-	b color.Color
+	a         color.Color
+	b         color.Color
+	Transform *matrix.Matrix
+}
+
+//Object struct
+type Object struct {
+	Transform *matrix.Matrix
+}
+
+//NewObject returns a new object
+func NewObject() Object {
+	return Object{
+		Transform: matrix.Identity,
+	}
 }
 
 //Stripe returns a stripe pattern alternating between colors a and b
 func Stripe(a color.Color, b color.Color) *Pattern {
-	return &Pattern{a: a, b: b}
+	return &Pattern{a: a, b: b, Transform: matrix.Identity}
 }
 
 //StripeAt returns the color of a stripe at a point
@@ -25,4 +39,13 @@ func (p *Pattern) StripeAt(point tuple.Tuple) *color.Color {
 		return &p.a
 	}
 	return &p.b
+}
+
+//StripeAtObject returns the color of a stripe at a point on an object
+func (p *Pattern) StripeAtObject(o Object, point tuple.Tuple) *color.Color {
+	oInv, _ := o.Transform.Inverse()
+	point = oInv.MultiplyTuple(point)
+	pInv, _ := p.Transform.Inverse()
+	point = pInv.MultiplyTuple(point)
+	return p.StripeAt(point)
 }
--- src/pattern/pattern_test.go
+++ src/pattern/pattern_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/calbim/ray-tracer/src/color"
+	"github.com/calbim/ray-tracer/src/transforms"
 	"github.com/calbim/ray-tracer/src/tuple"
 )
 
@@ -62,5 +63,36 @@ func TestStripeAlternatesInX(t *testing.T) {
 	}
 	if !pattern.StripeAt(tuple.Point(-1.1, 0, 0)).Equals(color.White) {
 		t.Errorf("wanted stripe at point %v=%v, got %v", tuple.Point(-1.1, 0, 0), color.White, pattern.StripeAt(tuple.Point(-1.1, 0, 0)))
+	}
+}
+
+func TestStripesWithObjectTransformation(t *testing.T) {
+	o := NewObject()
+	o.Transform = transforms.Scaling(2, 2, 2)
+	p := Stripe(color.White, color.Black)
+	c := p.StripeAtObject(o, tuple.Point(1.5, 0, 0))
+	if !c.Equals(color.White) {
+		t.Errorf("wanted c=%v, got %v", color.White, c)
+	}
+}
+
+func TestStripesWithPatternTransformation(t *testing.T) {
+	o := NewObject()
+	p := Stripe(color.White, color.Black)
+	p.Transform = transforms.Scaling(2, 2, 2)
+	c := p.StripeAtObject(o, tuple.Point(1.5, 0, 0))
+	if !c.Equals(color.White) {
+		t.Errorf("wanted c=%v, got %v", color.White, c)
+	}
+}
+
+func TestStripesWithObjectAndPatternTransformation(t *testing.T) {
+	o := NewObject()
+	o.Transform = transforms.Scaling(2, 2, 2)
+	p := Stripe(color.White, color.Black)
+	p.Transform = transforms.Translation(0.5, 0, 0)
+	c := p.StripeAtObject(o, tuple.Point(2.5, 0, 0))
+	if !c.Equals(color.White) {
+		t.Errorf("wanted c=%v, got %v", color.White, c)
 	}
 }
